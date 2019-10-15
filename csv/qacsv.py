475b4f01a0550575d18dc52243fbf79ac4eb7f75
Radu Sirghe <41892430+radusirghev@users.noreply.github.com>
2019-10-15 06:33:55 +0300

    csv-3

--- csv/qacsv.py
+++ csv/qacsv.py
@@ -3,26 +3,15 @@
 import csv
 import sys
 
-# exec_arg = sys.argv[1]
-exec_arg = "-a"
-# name_arg = sys.argv[2]
-name_arg = "Radu"
-# column_arg = sys.argv[3]
-column_arg = "age"
-# content_arg = sys.argv[4]
-content_arg = "25"
+exec_arg = sys.argv[1]
 
 
 class ReadCSV(object):
 
-    def __init__(self, text_file='/home/radumd/project/python/csv/data.csv'):
+    def __init__(self, text_file='/home/radu/Projects/python/csv/data.csv'):
         with open(text_file) as read_file:
             self.r_csv_file = read_file.readlines()
-            self.read_arg = exec_arg
-            self.name_arg = name_arg
-        self.text_file_to_write = text_file
-        self.column_arg = column_arg
-        self.content_arg = content_arg
+            self.name_arg = sys.argv[2]
 
     def csv_dict_reader(self):
         list_person = []
@@ -32,19 +21,25 @@ class ReadCSV(object):
         return list_person
 
     def show_person(self):
-        if self.read_arg is not None:
-            for person in self.csv_dict_reader():
-                if person['first_name'] == self.name_arg:
-                    return "Name: {} {}\nDepartament: {}\nAge: {}\nProjects: {}".format(person['first_name'], person['last_name'], person['departament'], person['age'], person['projects'])
+        for person in self.csv_dict_reader():
+            if person['first_name'] == self.name_arg:
+                return "Name: {} {}\nDepartament: {}\nPosition: {}\nAge: {}\nProjects: {}".format(person['first_name'],
+                                                                                    person['last_name'],
+                                                                                    person['departament'],
+                                                                                    person['position'],
+                                                                                    person['age'],
+                                                                                    person['projects'])
+            else:
+                return "Not a person by name \'" + self.name_arg + "\'"
 
 
 class ModifyCSV(ReadCSV):
 
-    def __init__(self, text_file='/home/radumd/project/python/csv/data.csv'):
+    def __init__(self, text_file='/home/radu/Projects/python/csv/data.csv'):
         super(ModifyCSV, self).__init__(text_file)
         self.text_file_to_write = text_file
-        self.column_arg = column_arg
-        self.content_arg = content_arg
+        self.column_arg = sys.argv[3]
+        self.content_arg = sys.argv[4]
 
     def modif_data_person(self):
         mod_list = []
@@ -57,7 +52,7 @@ class ModifyCSV(ReadCSV):
         return mod_list
 
     def add_modif_in_file(self):
-        key_list = ["first_name", "last_name", "departament", "age", "projects"]
+        key_list = ["first_name", "last_name", "departament", "position", "age", "projects"]
         with open(self.text_file_to_write, 'w') as w_file:
             w = csv.DictWriter(w_file, key_list)
             w.writeheader()
@@ -66,15 +61,15 @@ class ModifyCSV(ReadCSV):
         return "Modified!!"
 
 
-def res_function():
+def exec_function():
     if exec_arg == "-a":
-        c = ReadCSV()
-        print c.show_person()
-        print "Read"
-    elif exec_arg == "-m":
-        c = ModifyCSV()
-        print c.add_modif_in_file()
-        print "Write"
-    else: print "Incorect argument"
-# t = ModifyCSV()
-# print t.add_modif_in_file()
+        r = ReadCSV()
+        return r.show_person()
+    if exec_arg == "-m":
+        m = ModifyCSV()
+        return m.add_modif_in_file()
+    else:
+        return "Incorect argument"
+
+
+print(exec_function())
