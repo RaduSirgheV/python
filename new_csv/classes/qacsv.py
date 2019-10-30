42467a3de9cc304dad46c150e0789459222e43bd
radu <radu94tube@gmail.com>
2019-10-30 09:40:19 +0200

    new csv

---
+++ new_csv/classes/qacsv.py
@@ -0,0 +1,83 @@
+#!/usr/bin/python2.7
+
+import csv
+
+
+class ReadCSV(object):
+
+    def __init__(self, text_file):
+        with open(text_file) as read_file:
+            self.r_csv_file = csv.reader(read_file, delimiter=',')
+            self.list = []
+            for row in self.r_csv_file:
+                self.list.append(row)
+
+    def csv_dict_reader(self):
+        list_person = self.list[1:]
+        return list_person
+
+    def show_person(self, person_identifier=None):
+        employees_list = self.csv_dict_reader()
+        if person_identifier is None:
+            return employees_list
+        else:
+            displayed_person = []
+            for person in employees_list:
+                if person[0] == person_identifier:
+                    displayed_person.append(person)
+            if len(displayed_person) > 0:
+                for show_person in displayed_person:
+                    return "Name: {} {}\nDepartament: {}\nPosition: {}\nAge: {}\nProjects: {}".format(show_person[0], show_person[1], show_person[2], show_person[3], show_person[4], show_person[5])
+            elif len(displayed_person) == 0:
+                return "Not a person by name \'" + person_identifier + "\'"
+
+
+class ModifyCSV(ReadCSV):
+
+    def __init__(self, text_file):
+        ReadCSV.__init__(self, text_file)
+        self.text_file = text_file
+
+    def modif_data_person(self, args, key_list):
+        mod_list = []
+        for person in self.csv_dict_reader():
+            if person[0] == args[0]:
+                person[key_list.index(args[1])] = str(args[2])
+                mod_list.append(person)
+            else:
+                mod_list.append(person)
+        return mod_list
+
+    def add_modif_in_file(self, args):
+        key_list = ["first_name", "last_name", "departament", "position", "age", "projects"]
+        with open(self.text_file, 'w') as w_file:
+            w = csv.writer(w_file)
+            w.writerow(key_list)
+            for x in self.modif_data_person(args, key_list):
+                w.writerow(x)
+        return "Modified!!"
+
+
+class DeletePersonCSV(ReadCSV):
+
+    def __init__(self, text_file):
+        ReadCSV.__init__(self, text_file)
+        self.text_file = text_file
+
+    def del_person(self, person_id):
+        mod_list = []
+        for person in self.csv_dict_reader():
+            if person[0] != person_id:
+                mod_list.append(person)
+        return mod_list
+
+    def add_modif_in_file(self, person_identifier):
+        key_list = ["first_name", "last_name", "departament", "position", "age", "projects"]
+        with open(self.text_file, 'w') as w_file:
+            w = csv.writer(w_file)
+            w.writerow(key_list)
+            for x in self.del_person(person_identifier):
+                w.writerow(x)
+        return "Deleted!!"
+
+
---
+++ new_csv/main.py
@@ -0,0 +1,32 @@
+#! /usr/bin/python2
+
+import os,argparse
+from classes.qacsv import ReadCSV, ModifyCSV, DeletePersonCSV
+
+path = os.path.abspath(__file__)
+path = path.replace('main.py', '')
+_csv_file_path = path + 'file/data.csv'
+
+
+def exec_function():
+    parser = argparse.ArgumentParser(description='Process the task')
+    parser.add_argument('-a', '--afisa', type=str, default=None, required=False,
+                        help='Afiseaza un anumit angajat')
+    parser.add_argument('-m', '--modifica', nargs='+', type=str, default=None, required=False,
+                        help='Modifica datele unui anumit angajat')
+    parser.add_argument('-d', '--delete', type=str, default=None, required=False,
+                        help='Sterge un anumit angajat')
+
+    args = parser.parse_args()
+    if args.afisa is not None:
+        r = ReadCSV(_csv_file_path)
+        return r.show_person(args.afisa)
+    elif args.modifica is not None:
+        m = ModifyCSV(_csv_file_path)
+        return m.add_modif_in_file(args.modifica)
+    elif args.delete is not None:
+        d = DeletePersonCSV(_csv_file_path)
+        return d.add_modif_in_file(args.delete)
+
+
+print(exec_function())
